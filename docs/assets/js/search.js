faab5f1a300cdc3047cb575d39c19fcc4d621ae2
Pascal Iske <info@pascal-iske.de>
2018-01-05 22:24:44 +0100

    Release v2.0.0

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"name","url":"globals.html#name","classes":"tsd-kind-variable tsd-is-not-exported"},{"id":1,"kind":32,"name":"version","url":"globals.html#version","classes":"tsd-kind-variable tsd-is-not-exported"}]};
